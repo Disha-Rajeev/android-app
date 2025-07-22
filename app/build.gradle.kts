c01d0af8b055e2e1c4c847f60de197c2389d2d45
Disha Rajeev <disharajeev22@gmail.com>
2025-07-22 18:06:09 +0530

    Bumped up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.example.tmdbapp"
     minSdk = 21
     targetSdk = 34
-    versionCode = 6
-    versionName = "2"
+    versionCode = 7
+    versionName = "1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
